eb8faab756efeb067e5884ea99ac35e530fd4996
BarbourD <65624530+BarbourD@users.noreply.github.com>
2022-05-30 17:24:01 -0600

    WIP: added partials, stylesheets, and have index route

--- controllers/combos.js
+++ controllers/combos.js
@@ -1,9 +1,23 @@
 const router = require('express').Router();
-
 const Combo = require('../models/combo.js');
 
+router.get('/', (req, res) => {
+    Combo.find({}, (err, foundCombos) => {
+        res.render('/combos/index.ejs', {
+            combos: foundCombos
+        });
+    });
+});
 
 
+
+router.get('/:id', (req,res) => {
+    Combo.findById(req.params.id, (err, foundCombo) => {
+        res.render('combos/show.ejs', {
+            combo: foundCombo
+        });
+    });  
+}); 
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -16,16 +16,19 @@ mongoose.connect(process.env.DATABASE_URL, {
 
 const db = mongoose.connection;
 db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
-db.on('connected', () => console.log('mongo is conneded'));
+db.on('connected', () => console.log('mongo is connected'));
 db.on('disconnected', () => console.log('mongo is disconnected'));
 
 //MIDDLEWARE
+app.use(express.static('public'))
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended: false}));
 app.use('/combos', combosController);
 
+
 app.get('/', (req, res) => {
-    res.render(index.ejs);
+    res.render('index.ejs');
+   
 })
 
 
