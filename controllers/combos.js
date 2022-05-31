069e0692da451256173fe6ec492f7f8f2b45eeb5
BarbourD <65624530+BarbourD@users.noreply.github.com>
2022-05-30 19:19:09 -0600

    WIP: added routes

--- controllers/combos.js
+++ controllers/combos.js
@@ -1,20 +1,57 @@
 const router = require('express').Router();
+const { escapeRegExpChars } = require('ejs/lib/utils');
+const { append } = require('express/lib/response');
 const Combo = require('../models/combo.js');
-
+ 
+//index
 router.get('/', (req, res) => {
     Combo.find({}, (err, foundCombos) => {
-        res.render('/combos/index.ejs', {
+        res.render('combos/index.ejs', {
             combos: foundCombos
         });
     });
 });
 
+//new
+router.get('/new', (req, res) => {
+    res.render('combos/new.ejs');
+});
 
+//destroy
+router.delete('/:id', (req, res) => {
+    Combo.findByIdAndRemove(req.params.id, ( )=> {
+        res.redirect('/combos');
+    });
+});
 
-router.get('/:id', (req,res) => {
+//update
+router.put('/:id', (req, res) => {
+    Combo.findByIdAndUpdate(req.params.id, req.body, (err, updateCombo) => {
+        res.redirect(`/combos/${updateCombo._id}`);
+    })
+})
+
+//create
+router.post('/', (req, res) => {
+    Combo.create(req.body, (err, createdCombo) => {
+        // console.log(err, createdCombo);
+        res.redirect('/');
+    })
+})
+
+//edit
+router.get('/:id/edit', (req, res) => {
+    Combo.findById(req.params.id, (err, foundCombo) => {
+        res.render('combos/edit.ejs', {
+            combo: foundCombo,
+        })
+    })
+})
+//show
+router.get('/:id', (req, res) => {
     Combo.findById(req.params.id, (err, foundCombo) => {
         res.render('combos/show.ejs', {
-            combo: foundCombo
+            combo: foundCombo,
         });
     });  
 }); 
